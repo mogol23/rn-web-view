37ade8e09f0ae32fa9629bf9e286ab9bedc477ec
Ahmad <phpspaceship@gmail.com>
2022-09-14 18:38:26 +0700

    change cookies manager lib

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
-import CookieManager from '@react-native-community/cookies';
+import CookieManager from '@react-native-cookies/cookies';
 import { connect } from 'react-redux';
 import { APP_URL } from '@env';
 import { globalActions } from '../../redux/actions';
