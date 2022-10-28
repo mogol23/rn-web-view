e1f904452a0de5b730a4716a99f066d542b2b739
Ahmad <phpspaceship@gmail.com>
2022-10-28 15:45:14 +0700

    remove unused libs

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { NativeBaseProvider, StatusBar } from 'native-base';
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import Navigation from './navigations';
 import { store, storePersisted } from './redux';
-import { theme } from './utils';
 
 
 export default () => {
@@ -14,12 +13,10 @@ export default () => {
     <ReduxProvider store={store}>
       <PersistGate persistor={storePersisted}>
         <SafeAreaProvider>
-          <NativeBaseProvider theme={theme}>
             <NavigationContainer>
               <StatusBar />
               <Navigation />
             </NavigationContainer>
-          </NativeBaseProvider>
         </SafeAreaProvider>
       </PersistGate>
     </ReduxProvider>
