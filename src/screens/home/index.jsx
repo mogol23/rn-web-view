fabce39cacfa976fc73b83d130eedce30b5d2853
Ahmad <phpspaceship@gmail.com>
2023-08-21 11:53:05 +0700

    fix ios cookies

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,4 +1,9 @@
-import { APP_HAS_CAMERA, APP_URL, PUSH_NOTIFICATION_ENABLED, PUSH_NOTIFICATION_STORE_TOKEN_API_URL } from '@env';
+import {
+  APP_HAS_CAMERA,
+  APP_URL,
+  PUSH_NOTIFICATION_ENABLED,
+  PUSH_NOTIFICATION_STORE_TOKEN_API_URL,
+} from '@env';
 import CookieManager from '@react-native-cookies/cookies';
 import messaging from '@react-native-firebase/messaging';
 import React, { useEffect, useRef, useState } from 'react';
@@ -15,7 +20,9 @@ import { apiInstance, webViewLocalStorage } from '../../utils';
 const App = ({ global: globalProps, route, ...props }) => {
   const [tempUrl, setTempUrl] = useState();
   const webViewRef = useRef();
-  const [initScript, setInitScript] = useState(webViewLocalStorage.SAVE_FROM_WEB);
+  const [initScript, setInitScript] = useState(
+    webViewLocalStorage.SAVE_FROM_WEB,
+  );
   const [currentUrl, setCurrentUrl] = useState();
   useEffect(() => {
     const allKeys = globalProps?.webview ?? [];
@@ -23,13 +30,12 @@ const App = ({ global: globalProps, route, ...props }) => {
       setInitScript(webViewLocalStorage.SAVE_FROM_WEB);
     } else {
       const SAVE_FROM_RN = `(function() {
-        ${allKeys.map((n) => `localStorage.setItem(${n.key}, ${n.value});`)}
+        ${allKeys.map(n => `localStorage.setItem(${n.key}, ${n.value});`)}
       })();`;
 
       setInitScript(SAVE_FROM_RN);
     }
   }, [globalProps]);
-
 
   useEffect(() => {
     if (route.params?.action_url) {
@@ -37,29 +43,33 @@ const App = ({ global: globalProps, route, ...props }) => {
     } else {
       setTempUrl(undefined);
     }
-  }, [route.params])
+  }, [route.params]);
 
   const checkFCMToken = async () => {
     const token = await messaging().getToken();
-    console.log('cookie', cookiesHelper.toString(globalProps.cookies))
-    await apiInstance.post(PUSH_NOTIFICATION_STORE_TOKEN_API_URL, {
-      fcm_token: token
-    }, {
-      withCredentials: true,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Cookie': cookiesHelper.toString(globalProps.cookies)
-      }
-    })
-  }
+    console.log('cookie', cookiesHelper.toString(globalProps.cookies));
+    await apiInstance.post(
+      PUSH_NOTIFICATION_STORE_TOKEN_API_URL,
+      {
+        fcm_token: token,
+      },
+      {
+        withCredentials: false,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Cookie: cookiesHelper.toString(globalProps.cookies),
+        },
+      },
+    );
+  };
 
   const bootAll = async () => {
     if (PUSH_NOTIFICATION_ENABLED) {
       PushNotification.popInitialNotification();
       PushNotification.requestPermissions(['alert', 'badge', 'sound']);
       // PushNotification.checkPermissions((res) => console.log('notif permission', res)) //Check permissions
-      await messaging().getInitialNotification()
+      await messaging().getInitialNotification();
       messaging().requestPermission();
       await checkFCMToken();
     }
@@ -67,15 +77,15 @@ const App = ({ global: globalProps, route, ...props }) => {
 
     switch (Platform.OS) {
       case 'android':
-        await bootAndroid()
+        await bootAndroid();
         break;
       case 'ios':
-        await bootiOS()
+        await bootiOS();
         break;
       default:
         break;
     }
-  }
+  };
 
   const onAndroidBackPress = () => {
     if (webViewRef.current && webViewRef.current.canGoBack) {
@@ -83,7 +93,7 @@ const App = ({ global: globalProps, route, ...props }) => {
       return true;
     }
     return false;
-  }
+  };
 
   const bootAndroid = async () => {
     BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
@@ -93,14 +103,14 @@ const App = ({ global: globalProps, route, ...props }) => {
     if (PUSH_NOTIFICATION_ENABLED) {
       PushNotification.createChannel(
         {
-          channelId: "push-notification-1", // (required)
-          channelName: "push-notification-1", // (required)
+          channelId: 'push-notification-1', // (required)
+          channelName: 'push-notification-1', // (required)
         },
-        (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+        created => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
       );
     }
-    console.log('booted for android')
-  }
+    console.log('booted for android');
+  };
 
   const bootiOS = async () => {
     if (APP_HAS_CAMERA) {
@@ -110,32 +120,38 @@ const App = ({ global: globalProps, route, ...props }) => {
       messaging().registerDeviceForRemoteMessages();
     }
     console.log('booted for iOS');
-  }
+  };
 
   useEffect(() => {
     bootAll();
-    return () => Platform.select({
-      android: () => {
-        BackHandler.removeEventListener('hardwareBackPress');
-      }
-    })
+    return () =>
+      Platform.select({
+        android: () => {
+          BackHandler.removeEventListener('hardwareBackPress');
+        },
+      });
   }, []);
 
-  const onLoadEnd = async (syntheticEvent) => {
+  const onLoadEnd = async syntheticEvent => {
     const domain = url.extractSegments(APP_URL)?.[0];
     let cookies;
-    console.log('current url', currentUrl)
+    console.log('current url', currentUrl);
+    console.log('domain', domain);
     if (Platform.OS == 'ios') {
-      cookies = await CookieManager.getAll(true).then((res) => {
-        console.log('xxx', res)
-        const filtered = Object.keys(res).filter(key => {
-          return res[key].domain.includes(domain);
-        }).reduce((cur, key) => { return Object.assign(cur, { [key]: res[key] }) }, {});
-        return filtered
+      cookies = await CookieManager.getAll(true).then(res => {
+        console.log('cookies', res);
+        const filtered = Object.keys(res)
+          .filter(key => {
+            return res[key].domain.includes(domain);
+          })
+          .reduce((cur, key) => {
+            return Object.assign(cur, { [key]: res[key] });
+          }, {});
+        return filtered;
       });
     } else {
       cookies = await CookieManager.get(domain);
-      console.log('xxx', cookies)
+      console.log('cookies', cookies);
     }
 
     if (PUSH_NOTIFICATION_ENABLED) {
@@ -152,7 +168,9 @@ const App = ({ global: globalProps, route, ...props }) => {
         source={{
           uri: `${tempUrl ?? APP_URL}`,
           headers: {
-            Cookie: globalProps.cookies ? cookiesHelper.toString(globalProps.cookies) : '',
+            Cookie: globalProps.cookies
+              ? cookiesHelper.toString(globalProps.cookies)
+              : '',
           },
         }}
         cacheEnabled={true}
@@ -164,13 +182,19 @@ const App = ({ global: globalProps, route, ...props }) => {
         allowsInlineMediaPlayback
         scalesPageToFit
         useWebKit
-        sharedCookiesEnabled={true}
+        sharedCookiesEnabled={false}
         thirdPartyCookiesEnabled={true}
         startInLoadingState
         javaScriptEnabled={true}
         domStorageEnabled={true}
         renderLoading={() => (
-          <View style={{ width: viewport.width, height: viewport.height, alignItems: 'center', justifyContent: 'center' }}>
+          <View
+            style={{
+              width: viewport.width,
+              height: viewport.height,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
             <Grid />
           </View>
         )}
@@ -178,7 +202,7 @@ const App = ({ global: globalProps, route, ...props }) => {
         style={StyleSheet.absoluteFillObject}
         onMessage={webViewLocalStorage.handleOnMessage}
         injectedJavaScript={initScript}
-        onNavigationStateChange={(navState) => {
+        onNavigationStateChange={navState => {
           webViewRef.current.canGoBack = navState.canGoBack;
           setCurrentUrl(navState.url);
         }}
@@ -187,10 +211,10 @@ const App = ({ global: globalProps, route, ...props }) => {
       />
     </>
   );
-}
+};
 
 const mapStateToProps = ({ global }) => ({
-  global
-})
+  global,
+});
 
 export default connect(mapStateToProps)(App);
